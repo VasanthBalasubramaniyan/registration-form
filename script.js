828c8d944bde9788be649f8399cd048ce7dba44e
VasanthBalasubramaniyan <vambuvasanth124@gmail.com>
2024-12-28 21:03:04 +0530

    Completed

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function checkName() {
 }
 
 function checkEmail() {
-    const emailFormat = /^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$/;
+    const emailFormat = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     if (!emailFormat.test(email.value)) {
         email_error.textContent = "Invalid email address";
